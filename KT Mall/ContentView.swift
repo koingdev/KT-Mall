eaaf30d670563deb74ea9c3c8fa72353c138e6ec
Saingkoing SEA <sea.saingkoing@gmail.com>
2022-06-15 23:36:10 +0900

    Initial Commit

---
+++ KT Mall/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KT Mall
+//
+//  Created by KoingDev on 15/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KT Mall/KT_MallApp.swift
@@ -0,0 +1,17 @@
+//
+//  KT_MallApp.swift
+//  KT Mall
+//
+//  Created by KoingDev on 15/6/22.
+//
+
+import SwiftUI
+
+@main
+struct KT_MallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
